006a0a183cf61d5ad3773c99e57e301333a51550
Matheus Alves <theusic@outlook.com>
2024-09-12 10:54:30 -0300

    Add files via upload

--- exercicio 23.py
+++ exercicio 23.py
@@ -1,5 +1,6 @@
 n = int(input("Informe um valor: "))
-while n == 0:
+while n <= 0:
     n = int(input("Informe novamente o valor: "))
 for x in range (1,n+1):
-    print(x)
+    print(x, end = " ")
+
